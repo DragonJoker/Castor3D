6450bd52c4b48d8ea7250471be034f2498b736b1
Sylvain Doremus <dragonjoker59@hotmail.com>
2016-02-19 01:33:15 +0000

    Fixed CompilerConfig.hpp

--- source/Core/CastorUtils/Src/CompilerConfig.hpp
+++ source/Core/CastorUtils/Src/CompilerConfig.hpp
@@ -33,11 +33,11 @@ http://www.gnu.org/copyleft/lesser.txt.
 		|| !__has_feature( cxx_deleted_functions )
 		|| !__has_feature( cxx_variadic_templates ) )
 #		error "Your compiler is too old, consider an upgrade."
+#	endif
 #elif defined( __GNUG__)
 #	define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-#		if GCC_VERSION < 40900
-#			error "Your compiler is too old, consider an upgrade."
-#		endif
+#	if GCC_VERSION < 40900
+#		error "Your compiler is too old, consider an upgrade."
 #	endif
 #elif defined( __BORLANDC__ )
 #	warning "Theoretically supported compiler, but untested yet"
