d8c97b3537039d68c03dd137dab253558a991ec8
Sylvain Doremus <dragonjoker59@hotmail.com>
2016-06-09 16:29:25 +0100

    Added a .editorconfig file, so ne more bothering with spaces or tab... Fixed few things discovered using -pedantic

--- source/Core/Castor3D/Src/Castor3DPrerequisites.cpp
+++ source/Core/Castor3D/Src/Castor3DPrerequisites.cpp
@@ -6,7 +6,7 @@
 
 namespace Castor
 {
-	IMPLEMENT_EXPORTED_OWNED_BY( Castor3D::Engine, Engine );
-	IMPLEMENT_EXPORTED_OWNED_BY( Castor3D::RenderSystem, RenderSystem );
-	IMPLEMENT_EXPORTED_OWNED_BY( Castor3D::Scene, Scene );
+	IMPLEMENT_EXPORTED_OWNED_BY( Castor3D::Engine, Engine )
+	IMPLEMENT_EXPORTED_OWNED_BY( Castor3D::RenderSystem, RenderSystem )
+	IMPLEMENT_EXPORTED_OWNED_BY( Castor3D::Scene, Scene )
 }
