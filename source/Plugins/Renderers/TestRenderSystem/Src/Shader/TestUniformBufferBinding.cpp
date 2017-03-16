ae5cfa5e1e5457e036e696620aa1df4b805eadce
Sylvain Doremus <dragonjoker59@hotmail.com>
2017-03-16 15:59:59 +0000

    Fixed TestUniformBufferBinding.cpp, to make it have a size, so uniform buffers are initialised.

--- source/Plugins/Renderers/TestRenderSystem/Src/Shader/TestUniformBufferBinding.cpp
+++ source/Plugins/Renderers/TestRenderSystem/Src/Shader/TestUniformBufferBinding.cpp
@@ -1,5 +1,7 @@
 #include "Shader/TestUniformBufferBinding.hpp"
 
+#include <Shader/UniformBuffer.hpp>
+
 using namespace Castor3D;
 using namespace Castor;
 
@@ -8,6 +10,17 @@ namespace TestRender
 	TestUniformBufferBinding::TestUniformBufferBinding( UniformBuffer & p_ubo, ShaderProgram const & p_program )
 		: UniformBufferBinding{ p_ubo, p_program }
 	{
+		auto l_size = 0u;
+
+		for ( auto & l_variable : p_ubo )
+		{
+			auto l_stride = l_variable->size();
+			m_variables.push_back( { l_size, 0u } );
+			l_size += l_stride;
+		}
+
+		m_size = l_size;
+
 	}
 
 	TestUniformBufferBinding::~TestUniformBufferBinding()
