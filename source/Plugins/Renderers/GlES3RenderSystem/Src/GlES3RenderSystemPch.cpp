67f0c611c13bd7da809797455ef9873c5c48ba56
DragonJoker <dragonjoker59@hotmail.com>
2019-02-15 11:42:54 +0100

    Removed Src subfolders.

--- source/Plugins/Renderers/GlES3RenderSystem/Src/GlES3RenderSystemPch.cpp
+++
@@ -1,5 +0,0 @@
-#include "GlES3RenderSystemPch.hpp"
-
-#if defined( VLD_AVAILABLE )
-#	include <vld.h>
-#endif
--- source/Samples/internationalisation/CastorViewer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion enableextensions
-set LIST=
-for %%x in (..\GuiCommon\Src\*.cpp) do set LIST=!LIST! %%x
-for %%x in (..\CastorViewer\Src\*.cpp) do set LIST=!LIST! %%x
-xgettext -d CastorViewer -s --keyword=_ -p .\po\ -o CastorViewer.pot %LIST%
