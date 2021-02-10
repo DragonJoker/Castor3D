8a35bc3caac6fd5dcb40e63e6e21fd3d802a24e2
DragonJoker <dragonjoker59@hotmail.com>
2021-02-10 16:35:59 +0100

    Fixed GCC build.

--- include/Core/CastorUtils/Data/TextWriter.hpp
+++ include/Core/CastorUtils/Data/TextWriter.hpp
@@ -127,62 +127,17 @@ namespace castor
 		CU_API String tabs()const;
 
 		template< typename ValueT >
-		bool write( TextFile & file, String const & name, ValueT const & value )const
-		{
-			auto result = file.writeText( tabs() + name + cuT( " " ) ) > 0
-				&& TextWriter< ValueT >{ tabs() }( value, file )
-				&& file.writeText( cuT( "\n" ) ) > 0;
-			checkError( result, name.c_str() );
-			return result;
-		}
-
+		bool write( TextFile & file, String const & name, ValueT const & value )const;
 		template< typename ValueT >
-		bool write( TextFile & file, ValueT const & value )const
-		{
-			auto result = TextWriter< ValueT >{ tabs() }( value, file );
-			checkError( result, cuT( "" ) );
-			return result;
-		}
-
+		bool write( TextFile & file, ValueT const & value )const;
 		template< typename Value1T, typename Value2T >
-		bool write( TextFile & file, String const & name, Value1T const & value1, Value2T const & value2 )const
-		{
-			auto result = file.writeText( tabs() + name + cuT( " " ) ) > 0
-				&& TextWriter< Value1T >{ tabs() }( value1, file )
-				&& file.writeText( cuT( " " ) ) > 0
-				&& TextWriter< Value2T >{ tabs() }( value2, file )
-				&& file.writeText( cuT( "\n" ) ) > 0;
-			checkError( result, name.c_str() );
-			return result;
-		}
-
+		bool write( TextFile & file, String const & name, Value1T const & value1, Value2T const & value2 )const;
 		template< typename ValueT >
-		bool writeOpt( TextFile & file
-			, String const & name
-			, ValueT const & value
-			, ValueT const & comp )const
-		{
-			bool result{ true };
-
-			if ( value != comp )
-			{
-				result = write( file, name, value );
-			}
-
-			return result;
-		}
-
+		bool write( TextFile & file, String const & name, castor::RangedValue< ValueT > const & value )const;
 		template< typename ValueT >
-		bool write( TextFile & file, String const & name, castor::RangedValue< ValueT > const & value )const
-		{
-			return write( file, name, value.value() );
-		}
-
+		bool write( TextFile & file, String const & name, castor::ChangeTracked< ValueT > const & value )const;
 		template< typename ValueT >
-		bool write( TextFile & file, String const & name, castor::ChangeTracked< ValueT > const & value )const
-		{
-			return write( file, name, value.value() );
-		}
+		bool writeOpt( TextFile & file, String const & name, ValueT const & value, ValueT const & comp )const;
 
 	private:
 		String m_tabs;
@@ -319,5 +274,7 @@ namespace castor
 			, castor::TextFile & file )override;
 	};
 }
+
+#include "TextWriter.inl"
 
 #endif
