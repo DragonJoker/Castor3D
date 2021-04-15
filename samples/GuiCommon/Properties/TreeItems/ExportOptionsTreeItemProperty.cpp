3c66a42888c1837e33cfa2a928895cce087d4473
DragonJoker <dragonjoker59@hotmail.com>
2021-04-15 15:18:26 +0200

    SceneExporter: Added a new library to handle exports consistently.

--- samples/GuiCommon/Properties/TreeItems/ExportOptionsTreeItemProperty.cpp
+++ samples/GuiCommon/Properties/TreeItems/ExportOptionsTreeItemProperty.cpp
@@ -10,7 +10,7 @@ using namespace castor;
 namespace GuiCommon
 {
 	ExportOptionsTreeItemProperty::ExportOptionsTreeItemProperty( bool editable
-		, ExportOptions & options )
+		, exporter::ExportOptions & options )
 		: TreeItemProperty{ nullptr, editable }
 		, m_options{ options }
 	{
@@ -27,9 +27,13 @@ namespace GuiCommon
 		static wxString PROPERTY_CATEGORY_EXPORT_OPTIONS = _( "Export Options" );
 		static wxString PROPERTY_OPTION_SCALE = _( "Scale" );
 		static wxString PROPERTY_OPTION_SUBFOLDER = _( "Data in subfolder" );
+		static wxString PROPERTY_OPTION_SPLIT_SUBMESHES = _( "Split submeshes into meshes" );
+		static wxString PROPERTY_OPTION_RECENTER_SUBMESHES = _( "Recenter created meshes" );
 
 		addProperty( grid, PROPERTY_CATEGORY_EXPORT_OPTIONS );
 		addPropertyT( grid, PROPERTY_OPTION_SCALE, &m_options.scale );
 		addPropertyT( grid, PROPERTY_OPTION_SUBFOLDER, &m_options.dataSubfolders );
+		addPropertyT( grid, PROPERTY_OPTION_SPLIT_SUBMESHES, &m_options.splitPerMaterial );
+		addPropertyT( grid, PROPERTY_OPTION_RECENTER_SUBMESHES, &m_options.recenter );
 	}
 }
