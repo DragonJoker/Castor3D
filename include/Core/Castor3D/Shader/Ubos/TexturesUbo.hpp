91096c157eff3737c7510dfe0b6319d4c5daeb1a
DragonJoker <dragonjoker59@hotmail.com>
2021-04-27 00:03:15 +0200

    Castor3D: Reworked TexturesUbo as an sdw::StructInstance.

--- include/Core/Castor3D/Shader/Ubos/TexturesUbo.hpp
+++ include/Core/Castor3D/Shader/Ubos/TexturesUbo.hpp
@@ -8,20 +8,40 @@ See LICENSE file in root folder
 
 #include "Castor3D/Buffer/UniformBufferOffset.hpp"
 
+#include <ShaderWriter/CompositeTypes/StructInstance.hpp>
+#include <ShaderWriter/VecTypes/Vec4.hpp>
+
 namespace castor3d
 {
+	namespace shader
+	{
+		struct TextureData
+			: public sdw::StructInstance
+		{
+			C3D_API TextureData( sdw::ShaderWriter & writer
+				, ast::expr::ExprPtr expr
+				, bool enabled );
+			C3D_API TextureData & operator=( TextureData const & rhs );
+
+			C3D_API static ast::type::StructPtr makeType( ast::type::TypesCache & cache );
+			C3D_API static std::unique_ptr< sdw::Struct > declare( sdw::ShaderWriter & writer );
+
+			sdw::Array< sdw::UVec4 > config;
+
+		private:
+			using sdw::StructInstance::getMember;
+			using sdw::StructInstance::getMemberArray;
+		};
+	}
+
 	class TexturesUbo
 	{
 	public:
 		using Configuration = TexturesUboConfiguration;
 
 	public:
-		//!\~english	Name of the HDR configuration frame variable buffer.
-		//!\~french		Nom du frame variable buffer contenant la configuration du HDR.
 		C3D_API static castor::String const BufferTextures;
-		//!\~english	Name of the texture configurations indices frame variable.
-		//!\~french		Nom de la frame variable contenant les indices de configurations de textures.
-		C3D_API static castor::String const TexturesConfig;
+		C3D_API static castor::String const TextureData;
 	};
 }
 
@@ -32,7 +52,7 @@ namespace castor3d
 		, set\
 		, ast::type::MemoryLayout::eStd140\
 		, enabled };\
-	auto c3d_textureConfig = texturesUbo.declMember< sdw::UVec4 >( castor3d::TexturesUbo::TexturesConfig, 3u );\
+	auto c3d_textureData = texturesUbo.declStructMember< castor3d::shader::TextureData >( castor3d::TexturesUbo::TextureData );\
 	texturesUbo.end()
 
 #endif
