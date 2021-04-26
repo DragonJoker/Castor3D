85ba1561e0934e06879a00ea8ac8e38dd169d8fa
DragonJoker <dragonjoker59@hotmail.com>
2021-04-26 19:20:00 +0200

    Castor3D: Reworked ShadowMapUbo as an sdw::StructInstance.

--- source/Core/Castor3D/Shader/Ubos/ShadowMapUbo.cpp
+++ source/Core/Castor3D/Shader/Ubos/ShadowMapUbo.cpp
@@ -6,14 +6,89 @@
 #include "Castor3D/Scene/Light/Light.hpp"
 #include "Castor3D/Scene/Light/DirectionalLight.hpp"
 #include "Castor3D/Scene/SceneNode.hpp"
+#include "Castor3D/Shader/Shaders/GlslLight.hpp"
+#include "Castor3D/Shader/Shaders/GlslLighting.hpp"
 
 namespace castor3d
 {
+	//*********************************************************************************************
+
+	namespace shader
+	{
+		ShadowMapData::ShadowMapData( sdw::ShaderWriter & writer
+			, ast::expr::ExprPtr expr
+			, bool enabled )
+			: StructInstance{ writer, std::move( expr ), enabled }
+			, m_lightProjection{ getMember< sdw::Mat4 >( "lightProjection" ) }
+			, m_lightView{ getMember< sdw::Mat4 >( "lightView" ) }
+			, m_lightPosFarPlane{ getMember< sdw::Vec4 >( "lightPosFarPlane" ) }
+			, m_lightIndex{ getMember< sdw::UInt >( "lightIndex" ) }
+		{
+		}
+
+		ShadowMapData & ShadowMapData::operator=( ShadowMapData const & rhs )
+		{
+			StructInstance::operator=( rhs );
+			return *this;
+		}
+
+		ast::type::StructPtr ShadowMapData::makeType( ast::type::TypesCache & cache )
+		{
+			auto result = cache.getStruct( ast::type::MemoryLayout::eStd140
+				, "ShadowMapData" );
+
+			if ( result->empty() )
+			{
+				result->declMember( "lightProjection", ast::type::Kind::eMat4x4F );
+				result->declMember( "lightView", ast::type::Kind::eMat4x4F );
+				result->declMember( "lightPosFarPlane", ast::type::Kind::eVec4F );
+				result->declMember( "lightIndex", ast::type::Kind::eUInt );
+			}
+
+			return result;
+		}
+
+		std::unique_ptr< sdw::Struct > ShadowMapData::declare( sdw::ShaderWriter & writer )
+		{
+			return std::make_unique< sdw::Struct >( writer
+				, makeType( writer.getTypesCache() ) );
+		}
+
+		sdw::Vec4 ShadowMapData::worldToView( sdw::Vec4 const & pos )const
+		{
+			return m_lightView * pos;
+		}
+
+		sdw::Vec4 ShadowMapData::viewToProj( sdw::Vec4 const & pos )const
+		{
+			return m_lightProjection * pos;
+		}
+
+		sdw::Float ShadowMapData::getLinearisedDepth( sdw::Vec3 const & pos )const
+		{
+			return length( pos - m_lightPosFarPlane.xyz() ) / m_lightPosFarPlane.w();
+		}
+
+		DirectionalLight ShadowMapData::getDirectionalLight( LightingModel const & lighting )const
+		{
+			return lighting.getDirectionalLight( getWriter()->cast< sdw::Int >( m_lightIndex ) );
+		}
+
+		PointLight ShadowMapData::getPointLight( LightingModel const & lighting )const
+		{
+			return lighting.getPointLight( getWriter()->cast< sdw::Int >( m_lightIndex ) );
+		}
+
+		SpotLight ShadowMapData::getSpotLight( LightingModel const & lighting )const
+		{
+			return lighting.getSpotLight( getWriter()->cast< sdw::Int >( m_lightIndex ) );
+		}
+	}
+
+	//*********************************************************************************************
+
 	castor::String const ShadowMapUbo::BufferShadowMap = cuT( "ShadowMapCfg" );
-	castor::String const ShadowMapUbo::LightProjection = cuT( "c3d_lightProjection" );
-	castor::String const ShadowMapUbo::LightView = cuT( "c3d_lightView" );
-	castor::String const ShadowMapUbo::LightPosFarPlane = cuT( "c3d_lightPosFarPlane" );
-	castor::String const ShadowMapUbo::LightIndex = cuT( "c3d_lightIndex" );
+	castor::String const ShadowMapUbo::ShadowMapData = cuT( "c3d_shadowMapData" );
 
 	ShadowMapUbo::ShadowMapUbo( Engine & engine )
 		: m_engine{ engine }
@@ -68,4 +143,6 @@ namespace castor3d
 			data.lightView = view;
 		}
 	}
+
+	//*********************************************************************************************
 }
