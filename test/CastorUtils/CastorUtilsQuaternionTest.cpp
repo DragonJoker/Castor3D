c75ce1daf769f16735d9d4b43e756af2073a79e6
DragonJoker <dragonjoker59@hotmail.com>
2021-04-28 11:14:25 +0200

    Tests: Fixed a bit glm matrices initialisation.

--- test/CastorUtils/CastorUtilsQuaternionTest.cpp
+++ test/CastorUtils/CastorUtilsQuaternionTest.cpp
@@ -61,88 +61,99 @@ namespace Testing
 
 	void CastorUtilsQuaternionTest::TransformationMatrixComparison()
 	{
-		Logger::logInfo( cuT( "	Rotate on X" ) );
-
-		for ( auto r = 0; r < 360; r += 1 )
 		{
-			Matrix4x4f mtx( 1 );
-			matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 1, 0, 0 ), Angle::fromDegrees( float( r ) ) ) );
-			glm::mat4 mat;
-			mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 1, 0, 0 ) );
-			CT_EQUAL( mtx, mat );
-		}
+			CT_ON( cuT( "Rotate on X" ) );
 
-		Logger::logInfo( cuT( "	Rotate on Y" ) );
-
-		for ( auto r = 0; r < 360; r += 1 )
-		{
-			Matrix4x4f mtx( 1 );
-			matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 0, 1, 0 ), Angle::fromDegrees( float( r ) ) ) );
-			glm::mat4 mat;
-			mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 0, 1, 0 ) );
-			CT_EQUAL( mtx, mat );
-		}
-
-		Logger::logInfo( cuT( "	Rotate on Z" ) );
-
-		for ( auto r = 0; r < 360; r += 1 )
-		{
-			Matrix4x4f mtx( 1 );
-			matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 0, 0, 1 ), Angle::fromDegrees( float( r ) ) ) );
-			glm::mat4 mat;
-			mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 0, 0, 1 ) );
-			CT_EQUAL( mtx, mat );
-		}
-
-		Logger::logInfo( cuT( "	Rotate on All" ) );
-
-		for ( auto r = 0; r < 360; r += 1 )
-		{
-			Matrix4x4f mtx( 1 );
-			matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 1, 1, 1 ), Angle::fromDegrees( float( r ) ) ) );
-			glm::mat4 mat;
-			mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 1, 1, 1 ) );
-			CT_EQUAL( mtx, mat );
-		}
-
-#	if GLM_VERSION >= 95
-
-		Logger::logInfo( cuT( "	Rotation" ) );
-
-		for ( auto r = 0; r < 360; r += 1 )
-		{
-			Angle angle( Angle::fromDegrees( float( r ) ) );
-			glm::mat4 glm = glm::mat4_cast( glm::normalize( glm::angleAxis( float( angle.radians() ), glm::normalize( glm::vec3( 1.0f, 1.0f, 1.0f ) ) ) ) );
-			Matrix4x4f mtx;
-			Quaternion::fromAxisAngle( Point3f( 1.0f, 1.0f, 1.0f ), angle ).toMatrix( mtx );
-			CT_EQUAL( mtx, glm );
-		}
-
-		Logger::logInfo( cuT( "	Transform" ) );
-
-		for ( auto r = 0; r < 360; r += 1 )
-		{
-			Matrix4x4f mtx( 1 );
-			Angle angle( Angle::fromDegrees( float( r ) ) );
-			matrix::setTransform( mtx, Point3f( r, r, -r ), Point3f( 0.3f, 0.6f, 1.0f ), Quaternion::fromAxisAngle( Point3f( 1.0f, 1.0f, 1.0f ), angle ) );
-			glm::mat4 mat( 1 );
+			for ( auto r = 0; r < 360; r += 1 )
 			{
-				// Compute transformation matrix with glm
-				glm::mat4 rotation = glm::mat4_cast( glm::normalize( glm::angleAxis( float( angle.radians() ), glm::normalize( glm::vec3( 1.0f, 1.0f, 1.0f ) ) ) ) );
-				glm::mat4 translation = glm::translate( glm::mat4( 1.0f ), glm::vec3( r, r, -r ) );
-				glm::mat4 scale = glm::scale( glm::mat4( 1.0f ), glm::vec3( 0.3f, 0.6f, 1.0f ) );
-				mat = translation;
-				mat *= rotation;
-				mat *= scale;
-				CT_EQUAL( translation * rotation * scale, mat );
-				mat = glm::translate( glm::mat4( 1.0f ), glm::vec3( r, r, -r ) );
-				mat *= rotation;
-				mat = glm::scale( mat, glm::vec3( 0.3f, 0.6f, 1.0f ) );
-				CT_EQUAL( translation * rotation * scale, mat );
+				CT_WHEN( "Angle = " + castor::string::toString( r ) );
+				Matrix4x4f mtx( 1 );
+				matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 1, 0, 0 ), Angle::fromDegrees( float( r ) ) ) );
+				glm::mat4 mat = glm::identity< glm::mat4 >();
+				mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 1, 0, 0 ) );
+				CT_EQUAL( mtx, mat );
 			}
-			CT_EQUAL( mtx, mat );
 		}
+		{
+			CT_ON( cuT( "Rotate on Y" ) );
 
+			for ( auto r = 0; r < 360; r += 1 )
+			{
+				CT_WHEN( "Angle = " + castor::string::toString( r ) );
+				Matrix4x4f mtx( 1 );
+				matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 0, 1, 0 ), Angle::fromDegrees( float( r ) ) ) );
+				glm::mat4 mat = glm::identity< glm::mat4 >();
+				mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 0, 1, 0 ) );
+				CT_EQUAL( mtx, mat );
+			}
+		}
+		{
+			CT_ON( cuT( "Rotate on Z" ) );
+
+			for ( auto r = 0; r < 360; r += 1 )
+			{
+				CT_WHEN( "Angle = " + castor::string::toString( r ) );
+				Matrix4x4f mtx( 1 );
+				matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 0, 0, 1 ), Angle::fromDegrees( float( r ) ) ) );
+				glm::mat4 mat = glm::identity< glm::mat4 >();
+				mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 0, 0, 1 ) );
+				CT_EQUAL( mtx, mat );
+			}
+		}
+		{
+			CT_ON( cuT( "Rotate on All" ) );
+
+			for ( auto r = 0; r < 360; r += 1 )
+			{
+				CT_WHEN( "Angle = " + castor::string::toString( r ) );
+				Matrix4x4f mtx( 1 );
+				matrix::rotate( mtx, Quaternion::fromAxisAngle( Point3f( 1, 1, 1 ), Angle::fromDegrees( float( r ) ) ) );
+				glm::mat4 mat = glm::identity< glm::mat4 >();
+				mat = glm::rotate( mat, glm::radians( float( r ) ), glm::vec3( 1, 1, 1 ) );
+				CT_EQUAL( mtx, mat );
+			}
+		}
+#	if GLM_VERSION >= 95
+		{
+			CT_ON( cuT( "Rotation" ) );
+
+			for ( auto r = 0; r < 360; r += 1 )
+			{
+				CT_WHEN( "Angle = " + castor::string::toString( r ) );
+				Angle angle( Angle::fromDegrees( float( r ) ) );
+				glm::mat4 glm = glm::mat4_cast( glm::normalize( glm::angleAxis( float( angle.radians() ), glm::normalize( glm::vec3( 1.0f, 1.0f, 1.0f ) ) ) ) );
+				Matrix4x4f mtx;
+				Quaternion::fromAxisAngle( Point3f( 1.0f, 1.0f, 1.0f ), angle ).toMatrix( mtx );
+				CT_EQUAL( mtx, glm );
+			}
+		}
+		{
+			CT_ON( cuT( "Transform" ) );
+
+			for ( auto r = 0; r < 360; r += 1 )
+			{
+				CT_WHEN( "Angle = " + castor::string::toString( r ) );
+				Matrix4x4f mtx( 1 );
+				Angle angle( Angle::fromDegrees( float( r ) ) );
+				matrix::setTransform( mtx, Point3f( r, r, -r ), Point3f( 0.3f, 0.6f, 1.0f ), Quaternion::fromAxisAngle( Point3f( 1.0f, 1.0f, 1.0f ), angle ) );
+				glm::mat4 mat = glm::identity< glm::mat4 >();
+				{
+					// Compute transformation matrix with glm
+					glm::mat4 rotation = glm::mat4_cast( glm::normalize( glm::angleAxis( float( angle.radians() ), glm::normalize( glm::vec3( 1.0f, 1.0f, 1.0f ) ) ) ) );
+					glm::mat4 translation = glm::translate( glm::mat4( 1.0f ), glm::vec3( r, r, -r ) );
+					glm::mat4 scale = glm::scale( glm::mat4( 1.0f ), glm::vec3( 0.3f, 0.6f, 1.0f ) );
+					mat = translation;
+					mat *= rotation;
+					mat *= scale;
+					CT_EQUAL( translation * rotation * scale, mat );
+					mat = glm::translate( glm::mat4( 1.0f ), glm::vec3( r, r, -r ) );
+					mat *= rotation;
+					mat = glm::scale( mat, glm::vec3( 0.3f, 0.6f, 1.0f ) );
+					CT_EQUAL( translation * rotation * scale, mat );
+				}
+				CT_EQUAL( mtx, mat );
+			}
+		}
 #	endif
 	}
 
