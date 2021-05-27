42dcd335f71478de0389ecfd2eafaaffdfcdc97a
DragonJoker <dragonjoker59@hotmail.com>
2021-05-27 11:37:55 +0200

    Bloom: Updated post effect.

--- source/Plugins/PostEffects/BloomPostEffect/BlurPass.cpp
+++ source/Plugins/PostEffects/BloomPostEffect/BlurPass.cpp
@@ -4,25 +4,13 @@
 
 #include <Castor3D/Engine.hpp>
 #include <Castor3D/Buffer/UniformBufferPools.hpp>
-#include <Castor3D/Cache/SamplerCache.hpp>
-#include <Castor3D/Material/Texture/Sampler.hpp>
-#include <Castor3D/Material/Texture/TextureLayout.hpp>
-#include <Castor3D/Model/Vertex.hpp>
-#include <Castor3D/Render/RenderSystem.hpp>
-#include <Castor3D/Render/RenderTarget.hpp>
-#include <Castor3D/Render/RenderPassTimer.hpp>
 #include <Castor3D/Shader/Program.hpp>
 
 #include <CastorUtils/Graphics/Image.hpp>
 
-#include <ashespp/Buffer/VertexBuffer.hpp>
-#include <ashespp/Image/Image.hpp>
-#include <ashespp/Image/ImageView.hpp>
-#include <ashespp/RenderPass/RenderPass.hpp>
-#include <ashespp/RenderPass/RenderPassCreateInfo.hpp>
-#include <ashespp/RenderPass/SubpassDescription.hpp>
-
 #include <ShaderWriter/Source.hpp>
+
+#include <RenderGraph/RunnablePasses/RenderQuad.hpp>
 
 #include <numeric>
 
@@ -32,7 +20,7 @@ namespace Bloom
 {
 	namespace
 	{
-		std::unique_ptr< ast::Shader > getVertexProgram( castor3d::RenderSystem & renderSystem )
+		std::unique_ptr< ast::Shader > getVertexProgram()
 		{
 			using namespace sdw;
 			VertexWriter writer;
@@ -59,7 +47,7 @@ namespace Bloom
 			DifImgIdx,
 		};
 
-		std::unique_ptr< ast::Shader > getPixelProgram( castor3d::RenderSystem & renderSystem )
+		std::unique_ptr< ast::Shader > getPixelProgram()
 		{
 			using namespace sdw;
 			FragmentWriter writer;
@@ -169,214 +157,79 @@ namespace Bloom
 
 			return result;
 		}
-
-		std::vector< ashes::SamplerPtr > doCreateSamplers( castor3d::RenderDevice const & device
-			, uint32_t blurPassesCount )
-		{
-			std::vector< ashes::SamplerPtr > result;
-
-			for ( auto i = 0u; i < blurPassesCount; ++i )
-			{
-				result.push_back( device->createSampler( VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-					, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-					, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-					, VK_FILTER_NEAREST
-					, VK_FILTER_NEAREST
-					, VK_SAMPLER_MIPMAP_MODE_NEAREST
-					, float( i )
-					, float( i + 1u ) ) );
-			}
-
-			return result;
-		}
-
-		ashes::RenderPassPtr doCreateRenderPass( castor3d::RenderDevice const & device
-			, VkFormat format )
-		{
-			ashes::VkAttachmentDescriptionArray attaches
-			{
-				{
-					0u,
-					format,
-					VK_SAMPLE_COUNT_1_BIT,
-					VK_ATTACHMENT_LOAD_OP_CLEAR,
-					VK_ATTACHMENT_STORE_OP_STORE,
-					VK_ATTACHMENT_LOAD_OP_DONT_CARE,
-					VK_ATTACHMENT_STORE_OP_DONT_CARE,
-					VK_IMAGE_LAYOUT_UNDEFINED,
-					VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-				}
-			};
-			ashes::SubpassDescriptionArray subpasses;
-			subpasses.emplace_back( ashes::SubpassDescription
-				{
-					0u,
-					VK_PIPELINE_BIND_POINT_GRAPHICS,
-					{},
-					{ { 0u, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL } },
-					{},
-					ashes::nullopt,
-					{},
-				} );
-			ashes::VkSubpassDependencyArray dependencies
-			{
-				{
-					VK_SUBPASS_EXTERNAL,
-					0u,
-					VK_PIPELINE_STAGE_HOST_BIT,
-					VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-					VK_ACCESS_HOST_WRITE_BIT,
-					VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
-					VK_DEPENDENCY_BY_REGION_BIT,
-				},
-				{
-					0u,
-					VK_SUBPASS_EXTERNAL,
-					VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-					VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-					VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
-					VK_ACCESS_SHADER_READ_BIT,
-					VK_DEPENDENCY_BY_REGION_BIT,
-				},
-			};
-			ashes::RenderPassCreateInfo createInfo
-			{
-				0u,
-				std::move( attaches ),
-				std::move( subpasses ),
-				std::move( dependencies ),
-			};
-			auto result = device->createRenderPass( "BloomBlurPass"
-				, std::move( createInfo ) );
-			return result;
-		}
-
-		ashes::DescriptorSetLayoutPtr doCreateDescriptorLayout( castor3d::RenderDevice const & device )
-		{
-			ashes::VkDescriptorSetLayoutBindingArray bindings
-			{
-				castor3d::makeDescriptorSetLayoutBinding( 0u, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT ),
-				castor3d::makeDescriptorSetLayoutBinding( 1u, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT ),
-			};
-			auto result = device->createDescriptorSetLayout( "BloomBlurPass"
-				, std::move( bindings ) );
-			return result;
-		}
 	}
 
 	//*********************************************************************************************
 
-	BlurPass::Subpass::Subpass( castor3d::RenderDevice const & device
-		, VkFormat format
-		, ashes::ImageView const & srcView
-		, ashes::ImageView const & dstView
-		, ashes::RenderPass const & renderPass
-		, ashes::DescriptorSetPool const & descriptorPool
-		, ashes::PipelineLayout const & pipelineLayout
+	BlurPass::Subpass::Subpass( crg::FrameGraph & graph
+		, crg::FramePass const & previousPass
+		, castor3d::RenderDevice const & device
+		, crg::ImageViewId const & srcView
+		, crg::ImageViewId const & dstView
 		, VkExtent2D dimensions
-		, castor3d::ShaderModule const & vertexShader
-		, castor3d::ShaderModule const & pixelShader
+		, ashes::PipelineShaderStageCreateInfoArray const & stages
 		, castor3d::UniformBufferOffsetT< castor3d::GaussianBlur::Configuration > const & blurUbo
-		, uint32_t index )
+		, uint32_t index
+		, bool isVertical )
+		: pass{ graph.createPass( "BloomBlurPass" + std::to_string( index ) + ( isVertical ? "Y" : "X" )
+			, [this, &stages, dimensions, index]( crg::FramePass const & pass
+				, crg::GraphContext const & context
+				, crg::RunnableGraph & graph )
+			{
+				return crg::RenderQuadBuilder{}
+					.renderPosition( {} )
+					.renderSize( { dimensions.width >> ( index + 1 )
+						, dimensions.height >> ( index + 1 ) } )
+					.texcoordConfig( {} )
+					.program( ashes::makeVkArray< VkPipelineShaderStageCreateInfo >( stages ) )
+					.build( pass, context, graph );
+			} ) }
 	{
-		dimensions.width >>= ( index + 1 );
-		dimensions.height >>= ( index + 1 );
-		auto name = "BloomBlurPass" + std::to_string( index );
-
-		sampler = device.device->createSampler( name
-			, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-			, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-			, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-			, VK_FILTER_NEAREST
-			, VK_FILTER_NEAREST
-			, VK_SAMPLER_MIPMAP_MODE_NEAREST
-			, float( index )
-			, float( index + 1u ) );
-
-		auto & descriptorLayout = descriptorPool.getLayout();
-		descriptorSet = descriptorPool.createDescriptorSet( name );
-		blurUbo.createSizedBinding( *descriptorSet
-			, descriptorLayout.getBinding( 0u ) );
-		descriptorSet->createBinding( descriptorLayout.getBinding( 1u )
-			, srcView
-			, *sampler );
-		descriptorSet->update();
-
-		ashes::ImageViewCRefArray attaches{ dstView };
-		frameBuffer = renderPass.createFrameBuffer( name
-			, dimensions
-			, std::move( attaches ) );
-		
-		ashes::PipelineVertexInputStateCreateInfo inputState
-		{
-			0u,
-			ashes::VkVertexInputBindingDescriptionArray
-			{
-				{ 0u, sizeof( castor3d::NonTexturedQuad::Vertex ), VK_VERTEX_INPUT_RATE_VERTEX },
-			},
-			ashes::VkVertexInputAttributeDescriptionArray
-			{
-				{ 0u, 0u, VK_FORMAT_R32G32_SFLOAT, offsetof( castor3d::NonTexturedQuad::Vertex, position ) },
-			},
-		};
-
-		ashes::PipelineShaderStageCreateInfoArray shaderStages;
-		shaderStages.push_back( castor3d::makeShaderState( device, vertexShader ) );
-		shaderStages.push_back( castor3d::makeShaderState( device, pixelShader ) );
-
-		pipeline = device->createPipeline( name
-			, ashes::GraphicsPipelineCreateInfo
-			{
-				0u,
-				shaderStages,
-				inputState,
-				ashes::PipelineInputAssemblyStateCreateInfo{},
-				ashes::nullopt,
-				ashes::PipelineViewportStateCreateInfo
-				{
-					0u,
-					{ 1u, ashes::makeViewport( {}, dimensions ) },
-					{ 1u, ashes::makeScissor( {}, dimensions ) }
-				},
-				ashes::PipelineRasterizationStateCreateInfo{},
-				ashes::PipelineMultisampleStateCreateInfo{},
-				ashes::PipelineDepthStencilStateCreateInfo{ 0u, VK_FALSE, VK_FALSE },
-				ashes::PipelineColorBlendStateCreateInfo{},
-				ashes::nullopt,
-				pipelineLayout,
-				renderPass,
-			} );
+		pass.addDependency( previousPass );
+		blurUbo.createPassBinding( pass, 0u );
+		pass.addSampledView( srcView
+			, 1u
+			, {}
+			, crg::SamplerDesc{ VK_FILTER_NEAREST
+				, VK_FILTER_NEAREST
+				, VK_SAMPLER_MIPMAP_MODE_NEAREST
+				, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
+				, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
+				, VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
+				, 0.0f
+				, float( index )
+				, float( index + 1u ) } );
+		pass.addOutputColourView( dstView );
 	}
 
-	std::vector< BlurPass::Subpass > doCreateSubpasses( castor3d::RenderDevice const & device
-		, VkFormat format
-		, castor3d::TextureLayout const & srcImage
-		, castor3d::TextureLayout const & dstImage
-		, ashes::RenderPass const & renderPass
-		, ashes::DescriptorSetPool const & descriptorPool
-		, ashes::PipelineLayout const & pipelineLayout
+	std::vector< BlurPass::Subpass > doCreateSubpasses( crg::FrameGraph & graph
+		, crg::FramePassArray & previousPasses
+		, castor3d::RenderDevice const & device
+		, crg::ImageViewIdArray const & srcImages
+		, crg::ImageViewIdArray const & dstImages
 		, VkExtent2D dimensions
-		, castor3d::ShaderModule const & vertexShader
-		, castor3d::ShaderModule const & pixelShader
+		, ashes::PipelineShaderStageCreateInfoArray const & stages
 		, UboOffsetArray const & blurUbo
-		, uint32_t blurPassesCount )
+		, uint32_t blurPassesCount
+		, bool isVertical )
 	{
 		std::vector< BlurPass::Subpass > result;
+		assert( srcImages.size() == dstImages.size()
+			&& srcImages.size() == blurPassesCount );
 
 		for ( auto i = 0u; i < blurPassesCount; ++i )
 		{
-			result.emplace_back( device
-				, format
-				, srcImage.getMipView( i ).getSampledView()
-				, dstImage.getMipView( i ).getTargetView()
-				, renderPass
-				, descriptorPool
-				, pipelineLayout
+			result.emplace_back( graph
+				, *previousPasses[i]
+				, device
+				, srcImages[i]
+				, dstImages[i]
 				, dimensions
-				, vertexShader
-				, pixelShader
+				, stages
 				, blurUbo[i]
-				, i );
+				, i
+				, isVertical );
+			previousPasses[i] = &result.back().pass;
 		}
 
 		return result;
@@ -384,10 +237,11 @@ namespace Bloom
 
 	//*********************************************************************************************
 
-	BlurPass::BlurPass( castor3d::RenderDevice const & device
-		, VkFormat format
-		, castor3d::TextureLayout const & srcImage
-		, castor3d::TextureLayout const & dstImage
+	BlurPass::BlurPass( crg::FrameGraph & graph
+		, crg::FramePassArray const & previousPasses
+		, castor3d::RenderDevice const & device
+		, crg::ImageViewIdArray const & srcImages
+		, crg::ImageViewIdArray const & dstImages
 		, VkExtent2D dimensions
 		, uint32_t blurKernelSize
 		, uint32_t blurPassesCount
@@ -396,63 +250,44 @@ namespace Bloom
 		, m_blurKernelSize{ blurKernelSize }
 		, m_blurPassesCount{ blurPassesCount }
 		, m_blurUbo{ doCreateUbo( m_device, dimensions, m_blurKernelSize, m_blurPassesCount, isVertical ) }
-		, m_renderPass{ doCreateRenderPass( m_device, format ) }
-		, m_descriptorLayout{ doCreateDescriptorLayout( m_device ) }
-		, m_pipelineLayout{ m_device->createPipelineLayout( "BloomBlurPass" , *m_descriptorLayout ) }
-		, m_vertexShader{ VK_SHADER_STAGE_VERTEX_BIT, "BloomBlurPass", getVertexProgram( m_device.renderSystem ) }
-		, m_pixelShader{ VK_SHADER_STAGE_FRAGMENT_BIT, "BloomBlurPass", getPixelProgram( m_device.renderSystem ) }
-		, m_descriptorPool{ m_descriptorLayout->createPool( "BloomBlurPass", m_blurPassesCount ) }
-		, m_passes{ doCreateSubpasses( m_device
-			, format
-			, srcImage
-			, dstImage
-			, *m_renderPass
-			, *m_descriptorPool
-			, *m_pipelineLayout
+		, m_vertexShader{ VK_SHADER_STAGE_VERTEX_BIT, "BloomBlurPass", getVertexProgram() }
+		, m_pixelShader{ VK_SHADER_STAGE_FRAGMENT_BIT, "BloomBlurPass", getPixelProgram() }
+		, m_stages{ makeShaderState( device, m_vertexShader )
+			, makeShaderState( device, m_pixelShader ) }
+		, m_passes{ previousPasses }
+		, m_subpasses{ doCreateSubpasses( graph
+			, m_passes
+			, m_device
+			, srcImages
+			, dstImages
 			, dimensions
-			, m_vertexShader
-			, m_pixelShader
+			, m_stages
 			, m_blurUbo
-			, m_blurPassesCount ) }
+			, m_blurPassesCount
+			, isVertical ) }
 		, m_isVertical{ isVertical }
 	{
 	}
 
-	castor3d::CommandsSemaphoreArray BlurPass::getCommands( castor3d::RenderPassTimer const & timer
-		, uint32_t & index
-		, ashes::VertexBuffer< castor3d::NonTexturedQuad > const & vertexBuffer )const
+	BlurPass::BlurPass( crg::FrameGraph & graph
+		, crg::FramePass const & previousPass
+		, castor3d::RenderDevice const & device
+		, crg::ImageViewIdArray const & srcImages
+		, crg::ImageViewIdArray const & dstImages
+		, VkExtent2D dimensions
+		, uint32_t blurKernelSize
+		, uint32_t blurPassesCount
+		, bool isVertical )
+		: BlurPass{ graph
+			, { blurPassesCount, &previousPass }
+			, device
+			, srcImages
+			, dstImages
+			, dimensions
+			, blurKernelSize
+			, blurPassesCount
+			, isVertical }
 	{
-		castor3d::CommandsSemaphoreArray result;
-
-		for ( auto i = 0u; i < m_blurPassesCount; ++i )
-		{
-			auto & blur = m_passes[i];
-
-			ashes::CommandBufferPtr commandBuffer = m_device.graphicsCommandPool->createCommandBuffer( "BloomBlurPass" );
-			auto & cmd = *commandBuffer;
-
-			cmd.begin();
-			timer.beginPass( cmd, index );
-			cmd.beginDebugBlock( { "BloomBlurPass" + castor::string::toString( i )
-				, castor3d::makeFloatArray( m_device.renderSystem.getEngine()->getNextRainbowColour() ) } );
-			cmd.beginRenderPass( *m_renderPass
-				, *blur.frameBuffer
-				, { castor3d::transparentBlackClearColor }
-			, VK_SUBPASS_CONTENTS_INLINE );
-			cmd.bindPipeline( *blur.pipeline );
-			cmd.bindDescriptorSet( *blur.descriptorSet, *m_pipelineLayout );
-			cmd.bindVertexBuffer( 0u, vertexBuffer.getBuffer(), 0u );
-			cmd.draw( 6u );
-			cmd.endRenderPass();
-			cmd.endDebugBlock();
-			timer.endPass( cmd, index );
-			cmd.end();
-
-			++index;
-			result.emplace_back( std::move( commandBuffer ), m_device->createSemaphore( "BloomBlurPass" ) );
-		}
-
-		return result;
 	}
 
 	void BlurPass::accept( castor3d::PipelineVisitorBase & visitor )
