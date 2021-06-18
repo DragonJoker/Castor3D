a1c136401295543dcc37b5d8a758955ed9bb9521
DragonJoker <dragonjoker59@hotmail.com>
2021-06-18 23:04:35 +0200

    Castor3D: Updated use of crg::PipelineHolder.

--- include/Core/Castor3D/Render/GlobalIllumination/LightPropagationVolumes/GeometryInjectionPass.hpp
+++ include/Core/Castor3D/Render/GlobalIllumination/LightPropagationVolumes/GeometryInjectionPass.hpp
@@ -100,7 +100,6 @@ namespace castor3d
 
 	private:
 		class PipelineHolder
-			: public crg::PipelineHolder
 		{
 		public:
 			PipelineHolder( crg::FramePass const & pass
@@ -114,9 +113,10 @@ namespace castor3d
 				, uint32_t index );
 
 		protected:
-			void doCreatePipeline( uint32_t index )override;
+			void doCreatePipeline( uint32_t index );
 
 		private:
+			crg::PipelineHolder m_holder;
 			uint32_t m_lpvSize;
 			VkRenderPass m_renderPass;
 		};
