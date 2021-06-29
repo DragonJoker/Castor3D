06d98e51e34692de30e64448a1be19eb028eea2d
DragonJoker <dragonjoker59@hotmail.com>
2021-06-29 12:44:16 +0200

    Castor3D: Removed duplicate Fresnel/Schlick function.

--- include/Core/Castor3D/Shader/Shaders/GlslCookTorranceBRDF.hpp
+++ include/Core/Castor3D/Shader/Shaders/GlslCookTorranceBRDF.hpp
@@ -6,76 +6,71 @@ See LICENSE file in root folder
 
 #include "Castor3D/Shader/Shaders/GlslLighting.hpp"
 
-namespace castor3d
+namespace castor3d::shader
 {
-	namespace shader
+	class CookTorranceBRDF
 	{
-		class CookTorranceBRDF
-		{
-		public:
-			C3D_API explicit CookTorranceBRDF( sdw::ShaderWriter & writer );
-			C3D_API void declare();
-			C3D_API void declareDiffuse();
-			C3D_API void compute( Light const & light
-				, sdw::Vec3 const & worldEye
-				, sdw::Vec3 const & direction
-				, sdw::Vec3 const & specular
-				, sdw::Float const & metalness
-				, sdw::Float const & roughness
-				, Surface surface
-				, OutputComponents & output );
-			C3D_API sdw::Vec3 computeDiffuse( Light const & light
-				, sdw::Vec3 const & worldEye
-				, sdw::Vec3 const & direction
-				, sdw::Vec3 const & specular
-				, sdw::Float const & metalness
-				, Surface surface );
+	public:
+		C3D_API explicit CookTorranceBRDF( sdw::ShaderWriter & writer
+			, Utils & utils );
+		C3D_API void declare();
+		C3D_API void declareDiffuse();
+		C3D_API void compute( Light const & light
+			, sdw::Vec3 const & worldEye
+			, sdw::Vec3 const & direction
+			, sdw::Vec3 const & specular
+			, sdw::Float const & metalness
+			, sdw::Float const & roughness
+			, Surface surface
+			, OutputComponents & output );
+		C3D_API sdw::Vec3 computeDiffuse( Light const & light
+			, sdw::Vec3 const & worldEye
+			, sdw::Vec3 const & direction
+			, sdw::Vec3 const & specular
+			, sdw::Float const & metalness
+			, Surface surface );
 
-		protected:
-			void doDeclareDistribution();
-			void doDeclareGeometry();
-			void doDeclareFresnelShlick();
-			void doDeclareComputeCookTorrance();
-			void doDeclareComputeCookTorranceDiffuse();
+	protected:
+		void doDeclareDistribution();
+		void doDeclareGeometry();
+		void doDeclareComputeCookTorrance();
+		void doDeclareComputeCookTorranceDiffuse();
 
-		public:
-			sdw::ShaderWriter & m_writer;
-			sdw::Function< sdw::Float
-				, sdw::InFloat
-				, sdw::InFloat > m_distributionGGX;
-			sdw::Function< sdw::Float
-				, sdw::InFloat
-				, sdw::InFloat > m_geometrySchlickGGX;
-			sdw::Function< sdw::Float
-				, sdw::InFloat
-				, sdw::InFloat
-				, sdw::InFloat > m_geometrySmith;
-			sdw::Function< sdw::Float
-				, sdw::InVec3
-				, sdw::InVec3
-				, sdw::InVec3
-				, sdw::InFloat > m_smith;
-			sdw::Function< sdw::Vec3
-				, sdw::InFloat
-				, sdw::InVec3 > m_schlickFresnel;
-			sdw::Function< sdw::Void
-				, InLight
-				, sdw::InVec3
-				, sdw::InVec3
-				, sdw::InVec3
-				, sdw::InFloat
-				, sdw::InFloat
-				, InSurface
-				, OutputComponents & > m_computeCookTorrance;
-			sdw::Function< sdw::Vec3
-				, InLight
-				, sdw::InVec3
-				, sdw::InVec3
-				, sdw::InVec3
-				, sdw::InFloat
-				, InSurface > m_computeCookTorranceDiffuse;
-		};
-	}
+	public:
+		sdw::ShaderWriter & m_writer;
+		Utils & m_utils;
+		sdw::Function< sdw::Float
+			, sdw::InFloat
+			, sdw::InFloat > m_distributionGGX;
+		sdw::Function< sdw::Float
+			, sdw::InFloat
+			, sdw::InFloat > m_geometrySchlickGGX;
+		sdw::Function< sdw::Float
+			, sdw::InFloat
+			, sdw::InFloat
+			, sdw::InFloat > m_geometrySmith;
+		sdw::Function< sdw::Float
+			, sdw::InVec3
+			, sdw::InVec3
+			, sdw::InVec3
+			, sdw::InFloat > m_smith;
+		sdw::Function< sdw::Void
+			, InLight
+			, sdw::InVec3
+			, sdw::InVec3
+			, sdw::InVec3
+			, sdw::InFloat
+			, sdw::InFloat
+			, InSurface
+			, OutputComponents & > m_computeCookTorrance;
+		sdw::Function< sdw::Vec3
+			, InLight
+			, sdw::InVec3
+			, sdw::InVec3
+			, sdw::InVec3
+			, sdw::InFloat
+			, InSurface > m_computeCookTorranceDiffuse;
+	};
 }
 
 #endif
