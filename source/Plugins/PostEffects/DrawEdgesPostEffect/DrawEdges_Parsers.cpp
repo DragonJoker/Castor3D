8cda4f1e70875a89e0d553560d9f7a5c9434e1fd
DragonJoker <dragonjoker59@hotmail.com>
2021-07-21 03:19:40 +0200

    DrawEdges: Implemented edge detection using depth, normals and object ID.

---
+++ source/Plugins/PostEffects/DrawEdgesPostEffect/DrawEdges_Parsers.cpp
@@ -0,0 +1,82 @@
+#include "DrawEdgesPostEffect/DrawEdges_Parsers.hpp"
+
+#include "DrawEdgesPostEffect/DrawEdgesPostEffect.hpp"
+#include "DrawEdgesPostEffect/DrawEdgesUbo.hpp"
+
+#include <Castor3D/Engine.hpp>
+#include <Castor3D/Cache/TargetCache.hpp>
+#include <Castor3D/Render/RenderTarget.hpp>
+#include <Castor3D/Scene/SceneFileParser.hpp>
+
+namespace draw_edges
+{
+	struct ParserContext
+	{
+		castor3d::Engine * engine{ nullptr };
+		DrawEdgesUboConfiguration data{};
+	};
+
+	ParserContext & getParserContext( castor::FileParserContextSPtr context )
+	{
+		return *static_cast< ParserContext * >( context->getUserContext( PostEffect::Type ) );
+	}
+
+	CU_ImplementAttributeParser( parserDrawEdges )
+	{
+		ParserContext * deContext = new ParserContext;
+		deContext->engine = std::static_pointer_cast< castor3d::SceneFileContext >( context )->parser->getEngine();
+		context->registerUserContext( PostEffect::Type, deContext );
+	}
+	CU_EndAttributePush( Section::eRoot )
+
+	CU_ImplementAttributeParser( parserNormalDepthWidth )
+	{
+		auto & deContext = getParserContext( context );
+
+		if ( params.empty() )
+		{
+			CU_ParsingError( "Missing parameter" );
+		}
+		else
+		{
+			float value{};
+			params[0]->get( value );
+			deContext.data.normalDepthWidth = value;
+		}
+	}
+	CU_EndAttribute()
+
+	CU_ImplementAttributeParser( parserObjectWidth )
+	{
+		auto & deContext = getParserContext( context );
+
+		if ( params.empty() )
+		{
+			CU_ParsingError( "Missing parameter" );
+		}
+		else
+		{
+			float value{};
+			params[0]->get( value );
+			deContext.data.objectWidth = value;
+		}
+	}
+	CU_EndAttribute()
+
+	CU_ImplementAttributeParser( parserDrawEdgesEnd )
+	{
+		auto parsingContext = std::static_pointer_cast< castor3d::SceneFileContext >( context );
+		auto & deContext = getParserContext( context );
+		auto engine = deContext.engine;
+		castor3d::Parameters parameters;
+		parameters.add( PostEffect::NormalDepthWidth, castor::string::toString( deContext.data.normalDepthWidth ) );
+		parameters.add( PostEffect::ObjectWidth, castor::string::toString( deContext.data.objectWidth ) );
+		auto effect = engine->getRenderTargetCache().getPostEffectFactory().create( PostEffect::Type
+			, *parsingContext->renderTarget
+			, *engine->getRenderSystem()
+			, parameters );
+		parsingContext->renderTarget->addPostEffect( effect );
+		delete reinterpret_cast< ParserContext * >( context->unregisterUserContext( PostEffect::Type ) );
+	}
+	CU_EndAttributePop()
+}
