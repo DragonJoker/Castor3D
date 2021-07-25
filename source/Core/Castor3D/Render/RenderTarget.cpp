9df65246d29046f53c9f8280cad3588c429cca0e
DragonJoker <dragonjoker59@hotmail.com>
2021-07-25 22:41:31 +0200

    Castor3D: Added transfer src to "Objects" RenderTarget's image usage flags.

--- source/Core/Castor3D/Render/RenderTarget.cpp
+++ source/Core/Castor3D/Render/RenderTarget.cpp
@@ -272,6 +272,7 @@ namespace castor3d
 			, getPixelFormat()
 			, ( VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
 				| VK_IMAGE_USAGE_SAMPLED_BIT
+				| VK_IMAGE_USAGE_TRANSFER_SRC_BIT
 				| VK_IMAGE_USAGE_TRANSFER_DST_BIT )
 			, VK_BORDER_COLOR_FLOAT_OPAQUE_BLACK }
 		, m_overlays{ *getOwner()->getRenderSystem()->getMainRenderDevice()
