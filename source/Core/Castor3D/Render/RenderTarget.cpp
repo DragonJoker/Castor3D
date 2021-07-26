22b6a2241e383984c6c33de53fce415c40f4e6c4
DragonJoker <dragonjoker59@hotmail.com>
2021-07-27 00:42:51 +0200

    Castor3D: Fixed expected layout for velocity intermediate view.

--- source/Core/Castor3D/Render/RenderTarget.cpp
+++ source/Core/Castor3D/Render/RenderTarget.cpp
@@ -637,7 +637,7 @@ namespace castor3d
 			, TextureFactors{}.invert( true ) );
 		result.emplace_back( "Target Velocity"
 			, m_velocity
-			, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
+			, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
 			, TextureFactors{}.invert( true ) );
 
 		for ( auto & postEffect : m_hdrPostEffects )
