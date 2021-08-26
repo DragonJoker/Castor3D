91d1bbd93e4211320dbee41fa4fec25f1d3ee31c
DragonJoker <dragonjoker59@hotmail.com>
2021-08-26 12:30:01 +0200

    CastorUtils: Created SpinMutex implementation

--- include/Core/CastorUtils/Multithreading/MultithreadingModule.hpp
+++ include/Core/CastorUtils/Multithreading/MultithreadingModule.hpp
@@ -12,17 +12,34 @@ namespace castor
 	/**@name Multithreading */
 	//@{
 	/**
-	\~english
-	\brief		Thread pool implementation, using WorkerThreads.
-	\~french
-	\brief		Implémentation de pool de thread, utilisant des WorkerThread.
+	*\~english
+	*\brief
+	*	Atomic operators based spin lock implementation.
+	*\remarks
+	*	Uses the same interface as std::mutex.
+	*\~french
+	*\brief
+	*	Implémentation de spin lock basée sur les opérations atomiques.
+	*\remarks
+	*	Utilise la même interface que std::mutex.
+	*/
+	class SpinMutex;
+	/**
+	*\~english
+	*\brief
+	*	Thread pool implementation, using WorkerThreads.
+	*\~french
+	*\brief
+	*	Implémentation de pool de thread, utilisant des WorkerThread.
 	*/
 	class ThreadPool;
 	/**
-	\~english
-	\brief		Implementation of a worker thread to place in a thread pool.
-	\~french
-	\brief		Implàmentation d'un thread de travail à placer dans un pool de threads.
+	*\~english
+	*\brief
+	*	Implementation of a worker thread to place in a thread pool.
+	*\~french
+	*\brief
+	*	Implàmentation d'un thread de travail à placer dans un pool de threads.
 	*/
 	class WorkerThread;
 	//@}
