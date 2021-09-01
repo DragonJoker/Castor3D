afd167ab27e6742b73b943b827cf4eb291025c74
DragonJoker <dragonjoker59@hotmail.com>
2021-09-01 23:07:27 +0200

    Castor3D: Some GCC fixes.

--- source/Core/Castor3D/Event/Frame/FrameEventModule.cpp
+++ source/Core/Castor3D/Event/Frame/FrameEventModule.cpp
@@ -1,5 +1,11 @@
 #include "Castor3D/Event/Frame/FrameEventModule.hpp"
 
+#include "Castor3D/Event/Frame/CpuFrameEvent.hpp"
+#include "Castor3D/Event/Frame/CpuFunctorEvent.hpp"
+#include "Castor3D/Event/Frame/FrameListener.hpp"
+#include "Castor3D/Event/Frame/GpuFrameEvent.hpp"
+#include "Castor3D/Event/Frame/GpuFunctorEvent.hpp"
+
 CU_ImplementCUSmartPtr( castor3d, CpuFrameEvent )
 CU_ImplementCUSmartPtr( castor3d, FrameListener )
 CU_ImplementCUSmartPtr( castor3d, GpuFrameEvent )
