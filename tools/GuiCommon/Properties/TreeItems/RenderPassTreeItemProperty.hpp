2cb3dc3c6df763957f72b16bff7878bb28b387cc
DragonJoker <dragonjoker59@hotmail.com>
2021-12-13 13:07:25 +0100

    Castor3D: Added a way to visit an enumeration with names.

--- tools/GuiCommon/Properties/TreeItems/RenderPassTreeItemProperty.hpp
+++ tools/GuiCommon/Properties/TreeItems/RenderPassTreeItemProperty.hpp
@@ -10,43 +10,6 @@ See LICENSE file in root folder
 
 namespace GuiCommon
 {
-	/**
-	\~english
-	\brief		Helper class to communicate between Scene objects or Materials lists and PropertiesContainer.
-	\~french
-	\brief		Classe d'aide facilitant la communication entre la liste des objets de scène, ou la liste de matériaux, et PropertiesContainer.
-	*/
-	class RenderPassTreeItemProperty
-		: public TreeItemProperty
-		, private wxEvtHandler
-	{
-	public:
-		/**
-		 *\~english
-		 *\brief		Constructor
-		 *\param[in]	editable	Tells if the properties are modifiable.
-		 *\param[in]	rendePass	The target object.
-		 *\~french
-		 *\brief		Constructeur
-		 *\param[in]	editable	Dit si les propriétés sont modifiables.
-		 *\param[in]	rendePass	L'objet cible.
-		 */
-		RenderPassTreeItemProperty( bool editable
-			, castor3d::RenderTechniquePass & renderPass
-			, wxWindow * parent );
-
-	private:
-		/**
-		 *\copydoc GuiCommon::TreeItemProperty::doCreateProperties
-		 */
-		void doCreateProperties( wxPGEditor * editor
-			, wxPropertyGrid * grid )override;
-
-	private:
-		castor3d::RenderTechniquePass & m_renderPass;
-		wxWindow * m_parent;
-	};
-
 	void fillRenderPassConfiguration( wxPropertyGrid * grid
 		, TreeItemProperty & properties
 		, castor3d::RenderTechniquePass & renderPass );
