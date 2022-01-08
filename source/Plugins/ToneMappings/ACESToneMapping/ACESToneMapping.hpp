de355a2714eb52503591f3f39a8c0d0bfaf9628c
DragonJoker <dragonjoker59@hotmail.com>
2022-01-08 11:28:54 +0100

    Added ACES tone mapping plugin.

---
+++ source/Plugins/ToneMappings/ACESToneMapping/ACESToneMapping.hpp
@@ -0,0 +1,26 @@
+/*
+See LICENSE file in root folder
+*/
+#ifndef ___C3D_ACES_ToneMapping_H___
+#define ___C3D_ACES_ToneMapping_H___
+
+#include <Castor3D/Render/ToneMapping/ToneMapping.hpp>
+
+namespace aces
+{
+	/**
+	\~english
+	\brief		Implements Uncharted 2 tone mapping algorithm.
+	\~french
+	\brief		Implémente l'algorithme de mappage de ton d'Uncharted 2.
+	*/
+	struct ToneMapping
+	{
+		static castor::String Type;
+		static castor::String Name;
+		static castor3d::ShaderPtr create();
+	};
+}
+
+#endif
+
