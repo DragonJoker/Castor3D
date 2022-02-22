fa5d4cce049c73e92bb389b0fde5cc8214e69237
DragonJoker <dragonjoker59@hotmail.com>
2022-02-22 10:04:20 +0100

    Moved material and texture related buffers to global descriptor set.

--- source/Core/Castor3D/Render/Passes/PickingPass.cpp
+++ source/Core/Castor3D/Render/Passes/PickingPass.cpp
@@ -323,20 +323,20 @@ namespace castor3d
 		using namespace sdw;
 		FragmentWriter writer;
 		auto textureFlags = filterTexturesFlags( flags.textures );
+		bool hasTextures = !flags.textures.empty();
 
 		// UBOs
 		auto & renderSystem = *getEngine()->getRenderSystem();
-		auto materials = shader::createMaterials( writer, flags.passFlags );
-		materials->declare( uint32_t( NodeUboIdx::eMaterials )
-			, RenderPipeline::eBuffers );
-		bool hasTextures = !flags.textures.empty();
+		shader::Materials materials{ writer
+			, uint32_t( PassUboIdx::eMaterials )
+			, RenderPipeline::ePass };
 		shader::TextureConfigurations textureConfigs{ writer
-			, uint32_t( NodeUboIdx::eTexConfigs )
-			, RenderPipeline::eBuffers
+			, uint32_t( PassUboIdx::eTexConfigs )
+			, RenderPipeline::ePass
 			, hasTextures };
 		shader::TextureAnimations textureAnims{ writer
-			, uint32_t( NodeUboIdx::eTexAnims )
-			, RenderPipeline::eBuffers
+			, uint32_t( PassUboIdx::eTexAnims )
+			, RenderPipeline::ePass
 			, hasTextures };
 
 		auto c3d_maps( writer.declCombinedImgArray< FImg2DRgba32 >( "c3d_maps"
@@ -358,7 +358,7 @@ namespace castor3d
 			, [&]( FragmentInT< shader::FragmentSurfaceT > in
 			, FragmentOut out )
 			{
-				auto material = materials->getMaterial( in.material );
+				auto material = materials.getMaterial( in.material );
 				auto opacity = writer.declLocale( "opacity"
 					, material.opacity );
 				auto textureFlags = merge( flags.textures );
