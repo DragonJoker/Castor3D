8fe857bc950f9d697aabc8e3e4d4cd974dd9fb23
DragonJoker <dragonjoker59@hotmail.com>
2022-03-07 15:06:42 +0100

    Assimp: Improved import of materials properties.

--- source/Plugins/Importers/AssimpImporter/AssimpImporter.hpp
+++ source/Plugins/Importers/AssimpImporter/AssimpImporter.hpp
@@ -99,7 +99,8 @@ namespace C3dAssimp
 			, aiScene const & aiScene
 			, castor3d::Submesh & submesh );
 		castor3d::MaterialResPtr doProcessMaterial( castor3d::Scene & p_scene
-			, aiMaterial const & p_aiMaterial );
+			, aiMaterial const & p_aiMaterial
+			, uint32_t index );
 		castor3d::BoneSPtr doAddBone( castor::String const & p_name
 			, castor::Matrix4x4f const & p_offset
 			, castor3d::Skeleton & p_skeleton
