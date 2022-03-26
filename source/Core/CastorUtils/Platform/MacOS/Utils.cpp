045d95e1cac3891e676132e41ad54a3701af6248
DragonJoker <dragonjoker59@hotmail.com>
2022-03-26 16:25:17 +0100

    Added a way to retrieve the OS name.

--- source/Core/CastorUtils/Platform/MacOS/Utils.cpp
+++ source/Core/CastorUtils/Platform/MacOS/Utils.cpp
@@ -32,6 +32,34 @@ namespace castor
 
 			return strReturn;
 		}
+
+		String getOSName()
+		{
+			char name[128];
+			FILE * fp = popen( "sw_vers -productName", "r" );
+			auto read = fread( name, 1, sizeof( name ) - 1, fp );
+
+			if ( !read || read >= sizeof( name ) )
+			{
+				CU_Failure( "Couldn't retrieve OS name" );
+			}
+
+			pclose( fp );
+			char version[128];
+			fp = popen( "sw_vers -productVersion", "r" );
+			read = fread( version, 1, sizeof( version ) - 1, fp );
+
+			if ( !read || read >= sizeof( version ) )
+			{
+				CU_Failure( "Couldn't retrieve OS version" );
+			}
+
+			pclose( fp );
+			std::stringstream stream;
+			stream << name << " " << version;
+			auto result = stream.str();
+			return string::replace( result, "\n", "" );
+		}
 	}
 
 	void getLocaltime( std::tm * p_tm, time_t const * p_pTime )
