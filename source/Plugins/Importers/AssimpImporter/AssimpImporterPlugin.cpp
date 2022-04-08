e4a401cc8a9d43276056518c60dc7b1eca53fcce
DragonJoker <dragonjoker59@hotmail.com>
2022-04-08 02:45:17 +0200

    Assimp: Added glb extension for glTF support.

--- source/Plugins/Importers/AssimpImporter/AssimpImporterPlugin.cpp
+++ source/Plugins/Importers/AssimpImporter/AssimpImporterPlugin.cpp
@@ -82,7 +82,8 @@ namespace
 				extensions.emplace_back( cuT( "NDO" ), cuT( "Nendo Mesh" ) );
 				extensions.emplace_back( cuT( "OGEX" ), cuT( "open Game Engine Exchange" ) );
 				extensions.emplace_back( cuT( "UC" ), cuT( "Unreal" ) );
-				extensions.emplace_back( cuT( "GLTF" ), cuT( "glTF" ) );
+				extensions.emplace_back( cuT( "GLTF" ), cuT( "Text glTF" ) );
+				extensions.emplace_back( cuT( "GLB" ), cuT( "Binary glTF" ) );
 			}
 
 			std::set< castor::String > alreadyLoaded;
