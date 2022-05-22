ef16f1d00d678266996bc98d8eb17db699734ae5
DragonJoker <dragonjoker59@hotmail.com>
2022-05-22 19:36:56 +0200

    GuiCommon: Reworked animation state as a combo box.

--- tools/GuiCommon/Properties/TreeItems/AnimationTreeItemProperty.cpp
+++ tools/GuiCommon/Properties/TreeItems/AnimationTreeItemProperty.cpp
@@ -31,65 +31,60 @@ namespace GuiCommon
 		static wxString PROPERTY_ANIMATION_STOPPING_POINT = _( "Stop at" );
 		static wxString PROPERTY_ANIMATION_SPEED = _( "Speed" );
 		static wxString PROPERTY_ANIMATION_LOOPED = _( "Looped" );
-		static wxString PROPERTY_ANIMATION_STATE[] = { _( "Play" ), _( "Stop" ), _( "Pause" ) };
+		static wxString PROPERTY_ANIMATION_STATE = { _( "State" ) };
 
-		auto group = getGroup();
+		wxArrayString choices;
+		choices.Add( _( "Play" ) );
+		choices.Add( _( "Stop" ) );
+		choices.Add( _( "Pause" ) );
 
-		if ( group )
-		{
-			addProperty( grid, PROPERTY_CATEGORY_ANIMATION + make_wxString( m_name ) );
-			addProperty( grid, PROPERTY_ANIMATION_SPEED, m_groupAnim.scale
-				, [this]( wxVariant const & var )
+		addProperty( grid, PROPERTY_CATEGORY_ANIMATION + make_wxString( m_name ) );
+		addProperty( grid, PROPERTY_ANIMATION_SPEED, m_groupAnim.scale
+			, [this]( wxVariant const & var )
+			{
+				m_groupAnim.scale = variantCast< float >( var );
+				m_group.setAnimationScale( m_name, m_groupAnim.scale );
+			} );
+		addProperty( grid, PROPERTY_ANIMATION_STARTING_POINT, m_groupAnim.startingPoint
+			, [this]( wxVariant const & var )
+			{
+				m_groupAnim.startingPoint = variantCast< castor::Milliseconds >( var );
+				m_group.setAnimationStartingPoint( m_name, m_groupAnim.startingPoint );
+			} );
+		addProperty( grid, PROPERTY_ANIMATION_STOPPING_POINT, m_groupAnim.stoppingPoint
+			, [this]( wxVariant const & var )
+			{
+				m_groupAnim.stoppingPoint = variantCast< castor::Milliseconds >( var );
+				m_group.setAnimationStoppingPoint( m_name, m_groupAnim.stoppingPoint );
+			} );
+		addProperty( grid, PROPERTY_ANIMATION_LOOPED, m_groupAnim.looped
+			, [this]( wxVariant const & var )
+			{
+				m_groupAnim.looped = variantCast< bool >( var );
+				m_group.setAnimationLooped( m_name, m_groupAnim.looped );
+			} );
+		addProperty( grid, PROPERTY_ANIMATION_STATE, choices, choices[size_t( m_groupAnim.state )]
+			, [this]( wxVariant const & var )
+			{
+				auto state = castor3d::AnimationState( var.GetLong() );
+
+				switch ( state )
 				{
-					m_groupAnim.scale = variantCast< float >( var );
-					getGroup()->setAnimationScale( m_name, m_groupAnim.scale );
-				} );
-			addProperty( grid, PROPERTY_ANIMATION_STARTING_POINT, m_groupAnim.startingPoint
-				, [this]( wxVariant const & var )
-				{
-					m_groupAnim.startingPoint = variantCast< castor::Milliseconds >( var );
-					getGroup()->setAnimationStartingPoint( m_name, m_groupAnim.startingPoint );
-				} );
-			addProperty( grid, PROPERTY_ANIMATION_STOPPING_POINT, m_groupAnim.stoppingPoint
-				, [this]( wxVariant const & var )
-				{
-					m_groupAnim.stoppingPoint = variantCast< castor::Milliseconds >( var );
-					getGroup()->setAnimationStoppingPoint( m_name, m_groupAnim.stoppingPoint );
-				} );
-			addProperty( grid, PROPERTY_ANIMATION_LOOPED, m_groupAnim.looped
-				, [this]( wxVariant const & var )
-				{
-					m_groupAnim.looped = variantCast< bool >( var );
-					getGroup()->setAnimationLooped( m_name, m_groupAnim.looped );
-				} );
-			m_stateProperty = addProperty( grid, PROPERTY_ANIMATION_STATE[uint32_t( m_groupAnim.state )], editor
-				, static_cast< ButtonEventMethod >( [this]( wxVariant const & var )
-					{
-						auto group = getGroup();
-
-						switch ( m_groupAnim.state )
-						{
-						case castor3d::AnimationState::ePlaying:
-							group->pauseAnimation( m_name );
-							m_groupAnim.state = castor3d::AnimationState::ePaused;
-							break;
-
-						case castor3d::AnimationState::eStopped:
-							group->startAnimation( m_name );
-							m_groupAnim.state = castor3d::AnimationState::ePlaying;
-							break;
-
-						case castor3d::AnimationState::ePaused:
-							group->stopAnimation( m_name );
-							m_groupAnim.state = castor3d::AnimationState::eStopped;
-							break;
-
-						default:
-							break;
-						}
-
-						m_stateProperty->SetLabel( PROPERTY_ANIMATION_STATE[uint32_t( m_groupAnim.state )] );
-					} ) );
-		}
+				case castor3d::AnimationState::ePlaying:
+					m_group.startAnimation( m_name );
+					m_groupAnim.state = castor3d::AnimationState::ePlaying;
+					break;
+				case castor3d::AnimationState::eStopped:
+					m_group.stopAnimation( m_name );
+					m_groupAnim.state = castor3d::AnimationState::eStopped;
+					break;
+				case castor3d::AnimationState::ePaused:
+					m_group.pauseAnimation( m_name );
+					m_groupAnim.state = castor3d::AnimationState::ePaused;
+					break;
+				default:
+					break;
+				}
+			} );
 	}
 }
