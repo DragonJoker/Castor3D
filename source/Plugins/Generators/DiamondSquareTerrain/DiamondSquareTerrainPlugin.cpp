25b5fb214780a572782aaa3aac9af2e35fb6c019
DragonJoker <dragonjoker59@hotmail.com>
2022-06-21 13:00:22 +0200

    DiamondSquareTerrain: Added scene file section.

--- source/Plugins/Generators/DiamondSquareTerrain/DiamondSquareTerrainPlugin.cpp
+++ source/Plugins/Generators/DiamondSquareTerrain/DiamondSquareTerrainPlugin.cpp
@@ -1,7 +1,12 @@
 #include "DiamondSquareTerrain/DiamondSquareTerrain.hpp"
 
+#include "DiamondSquareTerrain/DiamondSquare_Parsers.hpp"
+
 #include <Castor3D/Engine.hpp>
 #include <Castor3D/Model/Mesh/MeshFactory.hpp>
+#include <Castor3D/Scene/SceneFileParser.hpp>
+
+#include <CastorUtils/FileParser/ParserParameter.hpp>
 
 #ifndef CU_PlatformWindows
 #	define C3D_DiamondSquareTerrain_API
@@ -12,6 +17,93 @@
 #		define C3D_DiamondSquareTerrain_API __declspec( dllimport )
 #	endif
 #endif
+
+namespace diamond_square_terrain
+{
+	namespace parser
+	{
+		static void parseError( castor::String const & error )
+		{
+			castor::StringStream stream{ castor::makeStringStream() };
+			stream << cuT( "Error, : " ) << error;
+			castor::Logger::logError( stream.str() );
+		}
+
+		static void addParser( castor::AttributeParsers & parsers
+			, uint32_t section
+			, castor::String const & name
+			, castor::ParserFunction function
+			, castor::ParserParameterArray && array = castor::ParserParameterArray{} )
+		{
+			auto nameIt = parsers.find( name );
+
+			if ( nameIt != parsers.end()
+				&& nameIt->second.find( section ) != nameIt->second.end() )
+			{
+				parseError( cuT( "Parser " ) + name + cuT( " for section " ) + castor::string::toString( section ) + cuT( " already exists." ) );
+			}
+			else
+			{
+				parsers[name][section] = { function, array };
+			}
+		}
+
+		static castor::AttributeParsers createParsers()
+		{
+			castor::AttributeParsers result;
+
+			addParser( result
+				, uint32_t( castor3d::CSCNSection::eMesh )
+				, Generator::Type
+				, &parserDiamondSquareTerrain );
+			addParser( result, uint32_t( DiamondSquareSection::eRoot )
+				, cuT( "}" )
+				, &parserDiamondSquareTerrainEnd );
+			addParser( result
+				, uint32_t( DiamondSquareSection::eRoot )
+				, Generator::ParamRoughness
+				, &parserRoughness
+				, { castor::makeParameter< castor::ParameterType::eFloat >() } );
+			addParser( result
+				, uint32_t( DiamondSquareSection::eRoot )
+				, Generator::ParamRandomSeed
+				, &parserRandomSeed
+				, { castor::makeParameter< castor::ParameterType::eBool >() } );
+			addParser( result
+				, uint32_t( DiamondSquareSection::eRoot )
+				, Generator::ParamScale
+				, &parserScale
+				, { castor::makeParameter< castor::ParameterType::eFloat >() } );
+			addParser( result
+				, uint32_t( DiamondSquareSection::eRoot )
+				, Generator::ParamDetail
+				, &parserDetail
+				, { castor::makeParameter< castor::ParameterType::eUInt32 >() } );
+			addParser( result
+				, uint32_t( DiamondSquareSection::eRoot )
+				, Generator::ParamMinY
+				, &parserMinY
+				, { castor::makeParameter< castor::ParameterType::eFloat >() } );
+
+			return result;
+		}
+
+		static castor::StrUInt32Map createSections()
+		{
+			return
+			{
+				{ uint32_t( DiamondSquareSection::eRoot ), Generator::Type },
+			};
+		}
+
+		static void * createContext( castor::FileParserContext & context )
+		{
+			auto userContext = new ParserContext;
+			userContext->engine = static_cast< castor3d::SceneFileParser * >( context.parser )->getEngine();
+			return userContext;
+		}
+	}
+}
 
 extern "C"
 {
@@ -38,6 +130,10 @@ extern "C"
 
 	C3D_DiamondSquareTerrain_API void OnLoad( castor3d::Engine * engine, castor3d::Plugin * p_plugin )
 	{
+		engine->registerParsers( diamond_square_terrain::Generator::Type
+			, diamond_square_terrain::parser::createParsers()
+			, diamond_square_terrain::parser::createSections()
+			, &diamond_square_terrain::parser::createContext );
 		engine->getMeshFactory().registerType( diamond_square_terrain::Generator::Type
 			, &diamond_square_terrain::Generator::create );
 	}
@@ -45,5 +141,6 @@ extern "C"
 	C3D_DiamondSquareTerrain_API void OnUnload( castor3d::Engine * engine )
 	{
 		engine->getMeshFactory().unregisterType( diamond_square_terrain::Generator::Type );
+		engine->unregisterParsers( diamond_square_terrain::Generator::Type );
 	}
 }
