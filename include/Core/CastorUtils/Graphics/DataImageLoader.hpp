720050b0f6db449f7a4c116f576a3db914b79a00
DragonJoker <dragonjoker59@hotmail.com>
2022-08-05 10:03:55 +0200

    Castor3D: Added a raw image data loader.

---
+++ include/Core/CastorUtils/Graphics/DataImageLoader.hpp
@@ -0,0 +1,35 @@
+/*
+See LICENSE file in root folder
+*/
+#ifndef ___CU_DataImageLoader_H___
+#define ___CU_DataImageLoader_H___
+
+#include "CastorUtils/Graphics/ImageLoader.hpp"
+
+namespace castor
+{
+	/**
+	\~english
+	\brief		Raw data image loader.
+	\remarks	Loads as a square 2D image.
+	\~french
+	\brief		Loader d'image à partir de données brutes.
+	\remarks	Charge en tant qu'image 2D carrée.
+	*/
+	class DataImageLoader
+		: public ImageLoaderImpl
+	{
+	public:
+		CU_API static void registerLoader( ImageLoader & reg );
+		CU_API static void unregisterLoader( ImageLoader & reg );
+		/**
+		 *\copydoc castor::ImageLoaderImpl::load
+		 */
+		CU_API ImageLayout load( String const & imageFormat
+			, uint8_t const * data
+			, uint32_t size
+			, PxBufferBaseSPtr & buffer )const override;
+	};
+}
+
+#endif
---
+++ source/Core/CastorUtils/Graphics/DataImageLoader.cpp
@@ -0,0 +1,50 @@
+#include "CastorUtils/Graphics/DataImageLoader.hpp"
+
+#include "CastorUtils/Data/LoaderException.hpp"
+#include "CastorUtils/Data/Path.hpp"
+#include "CastorUtils/Graphics/PixelBuffer.hpp"
+
+namespace castor
+{
+	namespace xpml
+	{
+		static StringArray const & listExtensions()
+		{
+			static StringArray const list
+			{
+#define CUPF_ENUM_VALUE( name, value, components, alpha, colour, depth, stencil, compressed )\
+				getFormatName( PixelFormat::e##name ),
+#include "CastorUtils/Graphics/PixelFormat.enum"
+			};
+			return list;
+		}
+
+		using R8G8B8Pixel = Pixel< PixelFormat::eR8G8B8_UNORM >;
+	}
+
+	void DataImageLoader::registerLoader( ImageLoader & reg )
+	{
+		reg.registerLoader( xpml::listExtensions()
+			, std::make_unique< DataImageLoader >() );
+	}
+
+	void DataImageLoader::unregisterLoader( ImageLoader & reg )
+	{
+		reg.unregisterLoader( xpml::listExtensions() );
+	}
+
+	ImageLayout DataImageLoader::load( String const & imageFormat
+		, uint8_t const * input
+		, uint32_t size
+		, PxBufferBaseSPtr & outbuffer )const
+	{
+		auto format = getFormatByName( imageFormat );
+		auto bitsize = getBytesPerPixel( format );
+		auto dim = uint32_t( sqrt( size / bitsize ) );
+		outbuffer = PxBufferBase::create( castor::Size{ dim, dim }
+			, format
+			, input
+			, format );
+		return ImageLayout{ ImageLayout::e2D, *outbuffer };
+	}
+}
