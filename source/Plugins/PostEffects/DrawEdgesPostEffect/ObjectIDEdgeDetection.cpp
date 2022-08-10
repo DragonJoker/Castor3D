859536f1fb76c8756bc97ec62b188a9bbb7fb172
DragonJoker <dragonjoker59@hotmail.com>
2022-08-10 16:23:58 +0200

    DrawEdges: Fixed nodeId check.

--- source/Plugins/PostEffects/DrawEdgesPostEffect/ObjectIDEdgeDetection.cpp
+++ source/Plugins/PostEffects/DrawEdgesPostEffect/ObjectIDEdgeDetection.cpp
@@ -140,15 +140,15 @@ namespace draw_edges
 						, ivec2( vec2( size ) * vtx_texture ) );
 					auto X = writer.declLocale( "X"
 						, c3d_data0.fetch( texelCoord, 0_i ) );
+					auto nodeId = writer.declLocale( "nodeId"
+						, writer.cast< sdw::UInt >( X.z() ) );
 
-					IF( writer, X.w() == 0.0_f )
+					IF( writer, nodeId == 0_u )
 					{
 						writer.demote();
 					}
 					FI;
 
-					auto nodeId = writer.declLocale( "nodeId"
-						, writer.cast< sdw::UInt >( X.z() ) );
 					auto modelData = writer.declLocale( "modelData"
 						, c3d_modelsData[writer.cast< sdw::UInt >( nodeId ) - 1u] );
 					auto material = writer.declLocale( "material"
