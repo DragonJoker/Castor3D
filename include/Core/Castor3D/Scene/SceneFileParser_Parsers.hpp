f99977793ac91ab11d6dd5bcdeb5aee21427f0b8
Sylvain Doremus <DragonJoker@users.noreply.github.com>
2023-01-21 11:52:42 +0100

    Castor3D: LightingModelComponent now handles default lighting model parsing.

--- include/Core/Castor3D/Scene/SceneFileParser_Parsers.hpp
+++ include/Core/Castor3D/Scene/SceneFileParser_Parsers.hpp
@@ -21,7 +21,6 @@ namespace castor3d
 	CU_DeclareAttributeParser( parserRootTextOverlay )
 	CU_DeclareAttributeParser( parserRootDebugOverlays )
 	CU_DeclareAttributeParser( parserRootWindow )
-	CU_DeclareAttributeParser( parserRootMaterials )
 	CU_DeclareAttributeParser( parserRootMaxImageSize )
 	CU_DeclareAttributeParser( parserRootLpvGridSize )
 	CU_DeclareAttributeParser( parserRootDefaultUnit )
