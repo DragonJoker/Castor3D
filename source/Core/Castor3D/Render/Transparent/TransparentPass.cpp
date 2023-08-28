6ec98b8f51735c8841e444c97ec5fd2584fe26d4
Sylvain Doremus <dragonjoker59@hotmail.com>
2023-08-28 16:00:51 +0200

    SSSSS: Reenabling backlit transmittance

--- source/Core/Castor3D/Render/Transparent/TransparentPass.cpp
+++ source/Core/Castor3D/Render/Transparent/TransparentPass.cpp
@@ -1,5 +1,6 @@
 #include "Castor3D/Render/Transparent/TransparentPass.hpp"
 
+#include "Castor3D/DebugDefines.hpp"
 #include "Castor3D/Engine.hpp"
 #include "Castor3D/Cache/ShaderCache.hpp"
 #include "Castor3D/Render/EnvironmentMap/EnvironmentMap.hpp"
@@ -215,7 +216,8 @@ namespace castor3d
 			, index };
 		shader::SssProfiles sssProfiles{ writer
 			, uint32_t( GlobalBuffersIdx::eSssProfiles )
-			, RenderPipeline::eBuffers };
+			, RenderPipeline::eBuffers
+			, !C3D_DisableSSSTransmittance };
 		shader::TextureConfigurations textureConfigs{ writer
 			, uint32_t( GlobalBuffersIdx::eTexConfigs )
 			, RenderPipeline::eBuffers
