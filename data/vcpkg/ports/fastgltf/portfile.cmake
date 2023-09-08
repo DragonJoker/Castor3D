881d5d7b342fa3235166403f0d2e26e1e4602a59
Sylvain Doremus <dragonjoker59@hotmail.com>
2023-09-08 10:06:05 +0200

    Submodule: Updated fastgltf.

--- data/vcpkg/ports/fastgltf/portfile.cmake
+++ data/vcpkg/ports/fastgltf/portfile.cmake
@@ -1,18 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO spnda/fastgltf
-    REF 90b4327f67a444f795dfc638e8066a9007b93a44
-    SHA512 20c747c5565d78cd4e7b7cd24638b9f23946cd538cec1e5d33c026583969e41180a439cfea71915f014b8fa30d763e9f59ae7c72db68bc056cffa747e3d2ac2d
+    REF 46f28f1355657c42c9f73bfcdde4ba37d05c2a2f
+    SHA512 8e26d8d8061a4416d0c2d934a74e8569d3d60db9f01fa7eeae9b251b61e151941c569ea8da70050746e241ca2f0cb4ff14809701f7247f9f31a8538ae28f5c3c
     HEAD_REF main
     PATCHES find_package.patch
             fix_uri_escape_spaces.patch
-            add_pbr_specular_glossiness_support.patch
-            add_gpu_instantiation_support.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS -DFASTGLTF_DOWNLOAD_SIMDJSON=OFF
+            -DFASTGLTF_ENABLE_DEPRECATED_EXT=ON
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
