3ae738a673805b1dfd2eeb8f77e4807755da8dd4
Sylvain Doremus <dragonjoker59@hotmail.com>
2024-05-07 10:31:42 +0200

    Viewer: Selected object boxes now use DebugDrawer.

--- tools/GuiCommon/System/CubeBoxManager.hpp
+++ tools/GuiCommon/System/CubeBoxManager.hpp
@@ -6,6 +6,9 @@ See LICENSE file in root folder
 #pragma once
 
 #include <Castor3D/Castor3DPrerequisites.hpp>
+
+#include <Castor3D/Buffer/GpuBufferOffset.hpp>
+
 #include <CastorUtils/Math/Angle.hpp>
 #include <CastorUtils/Math/Point.hpp>
 #include <CastorUtils/Math/Quaternion.hpp>
@@ -13,6 +16,11 @@ See LICENSE file in root folder
 
 namespace GuiCommon
 {
+	struct CubeBoxConfig
+	{
+		castor::Matrix4x4f world;
+		castor::Point4f colour;
+	};
 	/**
 	*\brief
 	*	Used to display the cube box of objects.
@@ -20,42 +28,29 @@ namespace GuiCommon
 	class CubeBoxManager
 	{
 	public:
-		explicit CubeBoxManager( castor3d::Scene & scene );
+		explicit CubeBoxManager( castor3d::RenderTarget const & renderTarget );
 		~CubeBoxManager();
 		void displayObject( castor3d::Geometry const & object
 			, castor3d::Submesh const & submesh );
 		void hideObject( castor3d::Geometry const & object );
 
 	private:
-		castor3d::GeometryRPtr doAddBB( castor3d::MeshResPtr bbMesh
-			, castor3d::MaterialObs material
-			, castor::String const & name
-			, castor3d::SceneNode & parent
-			, castor::BoundingBox const & bb
-			, bool handleSkeleton = true );
-		void doRemoveBB( castor3d::GeometryRPtr bbObject );
 		void onSceneUpdate( castor3d::Scene const & scene );
 
 	private:
-		castor3d::Scene & m_scene;
-		castor3d::MaterialObs m_obbMeshMaterial{};
-		castor3d::MaterialObs m_obbSubmeshMaterial{};
-		castor3d::MaterialObs m_obbSelectedSubmeshMaterial{};
-		castor3d::MaterialObs m_obbBoneMaterial{};
-		castor3d::MaterialObs m_aabbMeshMaterial{};
-		castor3d::MeshResPtr m_obbMesh{};
-		castor3d::MeshResPtr m_obbSubmesh{};
-		castor3d::MeshResPtr m_obbSelectedSubmesh{};
-		castor3d::MeshResPtr m_obbBone{};
-		castor3d::MeshResPtr m_aabbMesh{};
+		castor3d::RenderTarget const & m_renderTarget;
+		ashes::PipelineShaderStageCreateInfoArray m_program;
+		ashes::VkDescriptorSetLayoutBindingArray m_bindings;
+		ashes::WriteDescriptorSetArray m_writes;
+		castor::Point4f m_aabbMeshColour{};
+		castor::Point4f m_obbMeshColour{};
+		castor::Point4f m_obbSelectedSubmeshColour{};
+		castor::Point4f m_obbSubmeshColour{};
+		castor::Point4f m_obbBoneColour{};
 		castor3d::Geometry const * m_object{};
 		castor3d::Submesh const * m_submesh{};
-		castor3d::GeometryRPtr m_obbObject{};
-		std::vector< castor3d::GeometryRPtr > m_obbSubmeshObjects;
-		castor3d::GeometryRPtr m_obbSelectedSubmeshObject{};
-		std::vector< castor3d::GeometryRPtr > m_obbBoneObjects;
-		castor3d::GeometryRPtr m_aabbObject{};
 		castor3d::OnSceneUpdateConnection m_sceneConnection;
+		castor3d::GpuBufferOffsetT< CubeBoxConfig > m_cubeBoxBuffer;
 	};
 }
 
